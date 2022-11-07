3fab9edf65d08103a8dbb767f479fbe380bb73c6
Дмитрий Голосов <Dmitry.Golosov@urfu.me>
2022-11-07 09:35:07 +0500

    Print me fully

--- js/mock.js
+++ js/mock.js
@@ -1,18 +1,18 @@
 import { getRandomPositiveInteger} from '/utils.js';
-import {MASSAGES, PHOTOS, DISCRIPTION, NAMES, MaxCountPhotos } from '_/const.js';
+import {MASSAGES, createPhotoArray, DISCRIPTION, NAMES, MaxCountPhotos } from '_/const.js';
 const GetUserData = (id) => ({
-    id,
-    avatar: `img/avatar-${getRandomPositiveInteger(1, MaxCountPhotos)}.svg`,
-    message: MASSAGES[getRandomPositiveInteger(0, MASSAGES.length - 1)],
-    name: NAMES[getRandomPositiveInteger(0, NAMES.length - 1)],
+  id,
+  avatar: `img/avatar-${getRandomPositiveInteger(1, MaxCountPhotos)}.svg`,
+  message: MASSAGES[getRandomPositiveInteger(0, MASSAGES.length - 1)],
+  name: NAMES[getRandomPositiveInteger(0, NAMES.length - 1)],
 });
 
 const GetPhotoData = (id) => ({
-    id,
-    url: `photos/${id}.jpg`,
-    despription: DISCRIPTION[getRandomPositiveInteger(0, DISCRIPTION.length - 1)],
-    likes: getRandomPositiveInteger(CountLikes.MIN, CountLikes.MAX),
-    comments: Array.from({ length: getRandomPositiveInteger(1, 6) }).map((value, index) => GetUserData(index + 1)),
+  id,
+  url: `photos/${id}.jpg`,
+  despription: DISCRIPTION[getRandomPositiveInteger(0, DISCRIPTION.length - 1)],
+  likes: getRandomPositiveInteger(CountLikes.MIN, CountLikes.MAX),
+  comments: Array.from({ length: getRandomPositiveInteger(1, 6) }).map((value, index) => GetUserData(index + 1)),
 });
 const createPhotoArray = Array.from({ length: MaxCountPhotos }).map((value, index) => GetPhotoData(index + 1));
 const photos = createPhotoArray(MaxCountPhotos);
