5e1ed4cf6e95d4397c4eeef44e0895a062921e6f
Keks <keks@htmlacademy.ru>
2022-11-20 21:27:48 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,4 @@
+import { addPictures } from './thumbnail.js';
+
+addPictures();
+
---
+++ 7/js/thumbnail.js
@@ -0,0 +1,34 @@
+import { openPicture } from './big-picture.js';
+import { photos } from './mocks.js';
+
+const getPictureTemplate = ({id, url, comments, likes}) => `<a href="#" class="picture js-picture" data-id="${id}">
+<img class="picture__img" src="${url}" width="182" height="182" alt="Случайная фотография">
+<p class="picture__info">
+  <span class="picture__comments">${comments.length}</span>
+  <span class="picture__likes">${likes}</span>
+</p>
+</a>`;
+
+const renderThumbnails = () => {
+  const containerPictures = document.querySelector('.js-pictures');
+  containerPictures.insertAdjacentHTML('beforeend',photos.map((photo)=> getPictureTemplate(photo)).join(''));
+};
+
+const onPictureClick = (evt) => {
+  evt.preventDefault();
+  const target = evt.target;
+  const parent = target.closest('.js-picture');
+  const id = +parent.dataset.id;
+  openPicture(photos[id - 1]);
+};
+
+const addPictures = () => {
+  renderThumbnails();
+  const pictures = document.querySelectorAll('.js-picture');
+
+  pictures.forEach((picture) => {
+    picture.addEventListener('click', onPictureClick);
+  });
+};
+
+export { addPictures };
