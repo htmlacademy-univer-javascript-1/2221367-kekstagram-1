9533578d7cafe5ac1018e41db009eea477266a3d
Дмитрий Голосов <Dmitry.Golosov@urfu.me>
2022-12-18 23:47:50 +0500

    Need to pump up

--- js/api.js
+++ js/api.js
@@ -1,37 +1,37 @@
 const Urls = {
-    'GET': 'https://26.javascript.pages.academy/kekstagram/data',
-    'POST': 'https://26.javascript.pages.academy/kekstagram'
-  };
+  'GET': 'https://26.javascript.pages.academy/kekstagram/data',
+  'POST': 'https://26.javascript.pages.academy/kekstagram'
+};
   
-  const getData = (onSuccess, onFail) => {
-    fetch(Urls.GET)
-      .then((responce) => responce.json())
-      .then((photos) => {
-        onSuccess(photos);
-      })
-      .catch(() => {
+const getData = (onSuccess, onFail) => {
+  fetch(Urls.GET)
+    .then((responce) => responce.json())
+    .then((photos) => {
+      onSuccess(photos);
+    })
+    .catch(() => {
+      onFail();
+    });
+};
+  
+const sendData = (onSuccess, onFail, body) => {
+  fetch(
+    Urls.POST,
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
         onFail();
-      });
-  };
-  
-  const sendData = (onSuccess, onFail, body) => {
-    fetch(
-      Urls.POST,
-      {
-        method: 'POST',
-        body,
-      },
-    )
-      .then((response) => {
-        if (response.ok) {
-          onSuccess();
-        } else {
-          onFail();
-        }
-      })
-      .catch(() => {
-        onFail();
-      });
-  };
-  
-  export {getData, sendData};
+      }
+    })
+    .catch(() => {
+      onFail();
+  });
+};
+
+export {getData, sendData};
